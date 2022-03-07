9cbcbb8f3f34126a48c8ba155f6ba020bb93cf5c
Fabio Coelho <fbclh@protonmail.com>
2022-03-07 19:20:10 -0300

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+// import getAllData from './components/getDataFood.js';
+
+const beefsURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef';
+
+getAllData(beefsURL, 'Beef');
